4a321fa81ca7920481546f883409ce092e71726c
moshine <550612711@qq.com>
2020-09-23 16:28:31 +0800

    图标更改

--- app/src/main/java/com/wya/env/module/mine/MineFragment.java
+++ app/src/main/java/com/wya/env/module/mine/MineFragment.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.gson.Gson;
+import com.tencent.bugly.crashreport.CrashReport;
 import com.wya.env.App;
 import com.wya.env.R;
 import com.wya.env.base.BaseMvpFragment;
